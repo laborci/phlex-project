24ca4cc09657bb43b9f43535603873fbe5511cf3
Laborci Gergely <elvis@Laborcis-MacBook-Pro.local>
2017-07-25 00:09:50 +0200

    bower added

---
+++ App/Site/Website/@frontend/bower/bootstrap.js
@@ -0,0 +1,3 @@
+import '../../../../../public/bower/bootstrap/dist/js/bootstrap.min.js';
+var loadCss = require('load-css-file');
+loadCss('/bower/bootstrap/dist/css/bootstrap.min.css')
---
+++ App/Site/Website/@frontend/bower/fontawesome.js
@@ -0,0 +1,2 @@
+var loadCss = require('load-css-file');
+loadCss('/bower/font-awesome/css/font-awesome.min.css');
