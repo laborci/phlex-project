aa7f5e5c302bb502a5745800c8a93b5efad1f769
Laborci Gergely <elvis@Laborcis-MacBook-Pro.local>
2017-07-25 09:47:06 +0200

    no message

--- public/index.php
+++ public/index.php
@@ -8,8 +8,7 @@ require_once (__DIR__.'/../'.'autoload.php');
 session_start();
 
 Phlex\Routing\Launcher::launch(
-	\App\Site\Website\Site::class,
-	\App\Site\Admin\Site::class
+	\App\Site\Website\Site::class
 );
 
 // TODO: what to do?
