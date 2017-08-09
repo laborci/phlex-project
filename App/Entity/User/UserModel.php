6ac8ab4134607c17d4e5f01c4a306b5d6c5b1a33
Laborci Gergely <elvis@Laborcis-MacBook-Pro.local>
2017-08-09 08:41:20 +0200

    no message

--- App/Entity/User/UserModel.php
+++ App/Entity/User/UserModel.php
@@ -7,14 +7,15 @@ class UserModel extends \Phlex\RedFox\Model{
 	function repository() { return $this->repositoryFactory('user', 'database'); }
 
 	protected function fields(){
-		$this->addField('id', (new \Phlex\RedFox\Fields\IdField("int(11) unsigned"))->notNullable()->constant());
-		$this->addField('name', (new \Phlex\RedFox\Fields\StringField("varchar(255)")));
-		$this->addField('password', (new \Phlex\RedFox\Fields\PasswordField("char(32)", $this->passwordSalt)));
-		$this->addField('email', (new \Phlex\RedFox\Fields\StringField("varchar(255)")));
+		$this->hasField('id', (new \Phlex\RedFox\Fields\IdField("int(11) unsigned"))->notNullable()->constant());
+		$this->hasField('name', (new \Phlex\RedFox\Fields\StringField("varchar(255)")));
+		$this->hasField('password', (new \Phlex\RedFox\Fields\PasswordField("char(32)", $this->passwordSalt)));
+		$this->hasField('email', (new \Phlex\RedFox\Fields\StringField("varchar(255)")));
 	}
 
-	protected function decorateFields(){}
-	protected function relations(){}
+	protected function relations(){
+	}
+
 	protected function attachments(){
 		$this->hasAttachmentGroup('avatar')->acceptExtensions('jpg','png')->maxFileCount(1);
 	}
